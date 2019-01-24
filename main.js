4853835fdf7c7982450e1dd94112eab0d3825e2e
MattBurnsUk <hello@matt-burns.com>
2019-01-24 12:33:59 +0800

    added handlers and listeners for the bin icon. Added handlers and listeners for the check icon

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ var theList = document.getElementById("ul");
 var placeholder = document.createElement("LI");
 placeholder.setAttribute("id", "placeholderLi");
 placeholder.textContent = "Add a new item";
+var theBin = document.getElementsByClassName("bin-closed");
+var theChecks = document.getElementsByClassName("checkButton");
 
-//getElementById(placeholderLi).textContent = "Add a new item...";
-//maybe placeholder needs to getElementById and call a static, purpose built new item li in the html
+// Adding new items to the list
 inserter.oninput = function() {
     var newItem = inserter.value;
     placeholder.textContent = newItem;
@@ -21,37 +22,48 @@ inserter.onchange = function() {
     placeholder.textContent = "Add a new item";
 }
 
-/*
-
-inserter.addEventListener("input", addNewThing());
-
-
-function addNewThing() {
-    //console.log("working");
-    createNewLi.textContent = inserter.textContent;
-    theList.appendChild(createNewLi);
-}
-    
-    
-    
-    var theContent = document.getElementById("addNewItem").textContent;
-    
-    
-    
-    
-    var newInputArea = document.createElement("input");
-    var createNewLi = document.createElement("li");
-    
-    
-    var addLiContent = li.textContent = newInputArea.textContent;
-    
-    
-    theList.appendChild(addLiContent);
+// Event handlers for the bin icon
+for (let i=0; i<theBin.length; i++){
+    theBin[i].onmouseover = function(e){
+        e.target.src = "bin-open.png";
+    }
+    theBin[i].onmouseleave = function(e){
+        e.target.src = "bin-closed.png";
+    }
+    theBin[i].onclick = function(e){
+        let listItem = e.target.parentElement.parentElement;
+        theList.removeChild(listItem);
+    }
 }
 
-//inserter.addEventListener('click', addNewThing)
+//Functions for the image event handlers
+
+function changeCheckImg(e) {
+    console.log(e.target.src);
+    if (e.target.src == "http://localhost/to-do-list/unchecked.png") {
+        e.target.src = "http://localhost/to-do-list/checked.png";
+    } else {
+        if (e.target.parentElement.parentElement.firstChild.getAttribute("id") != "http://localhost/to-do-list/checked.png") {
+        e.target.src = "http://localhost/to-do-list/unchecked.png";
+        }
+    }
+}
 
 
+function changeCheckImgPerm(e) {
+  // e.target.removeEventListener("mouseleave", changeCheckImg);
+    if (e.target.src == "http://localhost/to-do-list/unchecked.png"){
+        e.target.src = "http://localhost/to-do-list/checked.png";
+        e.target.parentElement.parentElement.firstChild.setAttribute("id", "checked");
+    } else {
+        e.target.src = "http://localhost/to-do-list/unchecked.png";
+        e.target.parentElement.parentElement.firstChild.setAttribute("id", "somethingrandom");
+    }
+}
 
-
-*/
+//Applying the image event listeners
+for (let i=0; i<theChecks.length; i++){
+    theChecks[i].addEventListener("mouseover", changeCheckImg);
+    theChecks[i].addEventListener("mouseleave", changeCheckImg);
+    theChecks[i].addEventListener("click", changeCheckImgPerm);
+}
